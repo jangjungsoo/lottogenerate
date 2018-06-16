dd5817e0af786c57b230f672d1c2d812e57ac4e0
witcloud <witcloud@gmail.com>
2018-06-16 01:11:52 +0000

    Add logic

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,30 @@
 package project;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
+    private Random random = new Random(System.nanoTime());
+    
+    public int[] generate() {
+        int[] result = new int[6];
+        
+        for(int i = 0; i <6; i++) {
+            result[i] = random.nextInt(45);
+        }
+        
+        return result;
+    }
 
     /**
      * @param args
      */
     public static void main(String[] args) {
+        int[] result = new Main().generate();
+        System.out.println(Arrays.toString(result));
+        
         // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
+        //System.out.println("Hello goorm!");
     }
 
 }
